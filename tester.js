e4210faa59e36d5ba884377ecd757f1c48ba450b
ScriggerMania <kryospehereis@gmail.com>
2025-05-24 22:28:00 -0500

    Update tester.js

--- tester.js
+++ tester.js
@@ -1,5 +1,7 @@
-const li = document.currentScript.closest('li');
-if (li) li.remove();
+  const elements = document.getElementsByClassName("xss");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
+  }
 console.log("Hello");
 const style = document.createElement('style');
 style.textContent = `
