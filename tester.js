04f6edbe93296b45fd9d1b41ae5ed6073a4f5a3e
ScriggerMania <kryospehereis@gmail.com>
2025-05-24 22:21:03 -0500

    Create tester.js

---
+++ tester.js
@@ -0,0 +1,26 @@
+const li = document.currentScript.closest('li');
+if (li) li.remove();
+console.log("Hello");
+const style = document.createElement('style');
+style.textContent = `
+@keyframes stretchAndSquash {
+  0%, 100% { transform: scale(1.1, 0.9); }
+  50% { transform: scale(0.9, 1.1); }
+}
+.stretch-squash {
+  animation: 1s ease-in-out infinite stretchAndSquash;
+  position: fixed;
+  left: -15px;
+  bottom: -35px;
+  width: 200px;
+  height: 200px;
+  z-index: 999999999;
+  background-image: url(https://ScriggerMania.github.io/zip.png);
+  background-size: contain;
+}
+`;
+document.head.appendChild(style);
+
+const div = document.createElement('div');
+div.className = 'stretch-squash';
+document.body.appendChild(div);
